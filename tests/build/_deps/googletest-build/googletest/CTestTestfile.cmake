1cdc96c598fbc211e8a1ab54baec08726c7e9b72
tinkeringforfun <tinkeringforfun@users.noreply.github.com>
2026-02-15 04:28:20 +0000

    gitignore tests/build/ — remove build artifacts from repo

--- tests/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/furball-pr/tests/build/_deps/googletest-src/googletest
-# Build directory: /tmp/furball-pr/tests/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
