d7291b020ef86b138410d20a8448364484984c63
tinkeringforfun <tinkeringforfun@users.noreply.github.com>
2026-02-15 04:28:13 +0000

    fix CI: set LLVM_PROFILE_FILE for gtest coverage collection
    Source cargo llvm-cov env before running Google Tests so profraw
    files land in the target directory for coverage report generation.

---
+++ tests/build/CMakeFiles/furball_tests.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/tmp/furball-pr/tests/test_catalog.cpp" "CMakeFiles/furball_tests.dir/test_catalog.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_catalog.cpp.o.d"
+  "/tmp/furball-pr/tests/test_connection.cpp" "CMakeFiles/furball_tests.dir/test_connection.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_connection.cpp.o.d"
+  "/tmp/furball-pr/tests/test_datatypes.cpp" "CMakeFiles/furball_tests.dir/test_datatypes.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_datatypes.cpp.o.d"
+  "/tmp/furball-pr/tests/test_edge_cases.cpp" "CMakeFiles/furball_tests.dir/test_edge_cases.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_edge_cases.cpp.o.d"
+  "/tmp/furball-pr/tests/test_errors.cpp" "CMakeFiles/furball_tests.dir/test_errors.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_errors.cpp.o.d"
+  "/tmp/furball-pr/tests/test_execution.cpp" "CMakeFiles/furball_tests.dir/test_execution.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_execution.cpp.o.d"
+  "/tmp/furball-pr/tests/test_getdata.cpp" "CMakeFiles/furball_tests.dir/test_getdata.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_getdata.cpp.o.d"
+  "/tmp/furball-pr/tests/test_getfunctions.cpp" "CMakeFiles/furball_tests.dir/test_getfunctions.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_getfunctions.cpp.o.d"
+  "/tmp/furball-pr/tests/test_metadata.cpp" "CMakeFiles/furball_tests.dir/test_metadata.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_metadata.cpp.o.d"
+  "/tmp/furball-pr/tests/test_parameters.cpp" "CMakeFiles/furball_tests.dir/test_parameters.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_parameters.cpp.o.d"
+  "/tmp/furball-pr/tests/test_transactions.cpp" "CMakeFiles/furball_tests.dir/test_transactions.cpp.o" "gcc" "CMakeFiles/furball_tests.dir/test_transactions.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/tmp/furball-pr/tests/build/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/tmp/furball-pr/tests/build/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/build/CMakeFiles/furball_tests.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/furball_tests.dir/test_catalog.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_catalog.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_connection.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_connection.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_datatypes.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_datatypes.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_edge_cases.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_edge_cases.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_errors.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_errors.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_execution.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_execution.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_getdata.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_getdata.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_getfunctions.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_getfunctions.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_metadata.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_metadata.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_parameters.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_parameters.cpp.o.d"
+  "CMakeFiles/furball_tests.dir/test_transactions.cpp.o"
+  "CMakeFiles/furball_tests.dir/test_transactions.cpp.o.d"
+  "furball_tests"
+  "furball_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/furball_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
